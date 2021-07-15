7a080e0378f920506cf7fb0604c1870f266619da
Dipayan Deb <68664533+TheDipsMeister18@users.noreply.github.com>
2021-07-15 21:43:31 +0530

    Add files via upload

--- js/start.js
+++ js/start.js
@@ -2,6 +2,6 @@ function submitForm(event) {
 	event.preventDefault();
 	let name = document.getElementById('exampleFormControlTextarea1').value;
 	sessionStorage.setItem('name', name);
-	location.href = '/index.html';
+	location.href = 'https://thedipsmeister18.github.io/Quiz-Site/';
 	console.log(name);
 }
